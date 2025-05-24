ec74ed9ece419291b8fa8347b5d245fadc695a7f
Bander Wardi <58252867+banderwardi@users.noreply.github.com>
2025-05-25 02:24:48 +0300

    Update document.controller.js

--- app/controllers/document.controller.js
+++ app/controllers/document.controller.js
@@ -40,15 +40,15 @@ export function create(req, res) {
 
 // Retrieve all Documents from the database.
 export function findAll(req, res) {
-    // res.send([{name: db.databaseName}]);
-    // if (!req || !req.body.collection) {
-    //     return res.sendStatus(400).send({
-    //       message: "Data can not be empty!"
-    //     });
-    // }
+    // res.send([{name: db.secondaryOk}]);
+    if (!req || !req.body.collection) {
+        return res.sendStatus(400).send({
+          message: "Data can not be empty!"
+        });
+    }
     db.collection('users').find({}).toArray()
         .then(data => {
-        res.send([]);
+        res.send(data);
         })
         .catch(err => {
         res.sendStatus(500).send({
